beff2a304c30b76d40d831905f5a5d5749bc6e6f
Sudhanshu <sudhanshusah2005@gmail.com>
2025-08-31 16:32:22 +0530

    changes in package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,6 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
-
-  // const assignmentSeed = import.meta.env.VITE_ASSIGNMENT_SEED || '';
-  // const accentColor = generateColorFromSeed(assignmentSeed);
-  // const appStyle = {
-  //   '--accent-color-dynamic': accentColor,
-  // } as React.CSSProperties;
-
   return (
     <div>
       <Navbar />
